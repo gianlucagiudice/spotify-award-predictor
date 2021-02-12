bb7805ec400db856bbef72210e1b370c0ea10fd4
AuPath <marco.au.grassi98@protonmail.com>
2021-02-12 14:28:29 +0100

    train svm function

--- src/functions/decision_tree.R
+++ src/functions/decision_tree.R
@@ -11,3 +11,15 @@ train_decision_tree <- function(training_data){
 ### DECISION TREE SPECIFIC PLOTS
 
 
+train_svm <- function(training_data, cost_list, gamma_list) {
+    svm.trained_cv = train(award ~ .,
+                           data = training_data,
+                           method = "svmRadial",
+                           tuneGrid = expand.grid(
+                             C = cost_list,
+                             sigma = gamma_list
+                           ),
+                           num.threads = detectCores())
+}
+
+
