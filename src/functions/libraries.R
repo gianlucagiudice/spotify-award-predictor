31a541311548d5adde8e98cc5dbaf8565e7427ed
AuPath <marco.au.grassi98@protonmail.com>
2021-02-18 22:07:43 +0100

    spostato comando webshot a dopo che la libreria webshot viene caricata se prima non era installata.

--- src/functions/libraries.R
+++ src/functions/libraries.R
@@ -3,18 +3,18 @@ libraries = c("ggplot2", "readr", "GGally", "ggcorrplot", "FactoMineR",
               "dplyr", "caret", "ROCR", "doParallel", "factoextra",
               "htmlwidgets", "tibble", "rattle", "rpart.plot")
 
-webshot::install_phantomjs()
-
 if (INSTALL_LIBRARIES){
-  installed_libraries = rownames(installed.packages())
-  
-  for (library in libraries){
-    if (! (library %in% installed_libraries)){
-      install.packages(library, dependencies = TRUE, character.only = TRUE)
+    installed_libraries = rownames(installed.packages())
+    
+    for (library in libraries){
+        if (! (library %in% installed_libraries)){
+            install.packages(library, dependencies = TRUE, character.only = TRUE)
+        }
     }
-  }
 }
 
 for (library in libraries){
-  library(library, character.only = TRUE)
+    library(library, character.only = TRUE)
 }
+
+webshot::install_phantomjs()
